c55dca83415bc6398a27170a0473a705ebda583b
Chia-Wei Li <dreamcwli@gmail.com>
2017-06-30 09:46:56 +0800

    Update VMAP API for VSP

--- ad/src/main/java/com/viscovery/ad/api/VspService.java
+++ ad/src/main/java/com/viscovery/ad/api/VspService.java
@@ -5,7 +5,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface VspService {
-    @GET("api/vmap?platform=mobile&debug=0")
+    @GET("tag2ad/webapi/ads/v1/vmap?format=json&platform=mobile&cache=0")
     Call<VmapResponse> getVmap(
             @Query(value = "api_key", encoded = true) String apiKey,
             @Query(value = "video_url", encoded = true) String videoUrl);
